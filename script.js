0f81395b22fc2d7325ae7d9756f760e11db8707d
shatrughan mishra <shatrughanm485@gmail.com>
2026-01-01 23:54:05 +0530

    learn js

---
+++ script.js
@@ -0,0 +1,54 @@
+
+// function sum(a,b){
+//     return a+b
+// }
+
+// const finalSum = sum(4,5);
+// console.log(finalSum)
+
+// function getLength(str) {
+//     console.log("Original String:", str);
+//     console.log("Length:", str.length);
+// }
+
+// getLength("hello world");
+
+// function findIndexOf(str, target) {
+//     console.log("Original String:", str);
+//     console.log("Index:", str.indexOf(target));
+//     console.log("Index:", str.lastIndexOf(target));
+// }
+
+// findIndexOf("hello world", "world") // indexOf
+// findIndexOf("hello world world world", "world") // lastindexOf
+
+
+// function getSlice(str, start, end) {
+//     console.log("Original String:", str);
+//     console.log("After slice:", str.slice(start,end));
+// }
+
+// getSlice("Hello World", 0, 5);
+
+// const value = "otsutsuki"
+// let ans = value.substr(2,5)//sutsu
+// let ans2 = value.slice(2,5)//sut
+
+// console.log(ans)
+// console.log(ans2)
+
+function cutIt(str, startIndex, endIndex) {
+    let newStr = "";
+    for(let i = 0; i<str.length; i++){
+        if(i>= startIndex && i<endIndex){
+            newStr = newStr + str[i]
+        }
+    }
+    return newStr
+}
+
+const value = "otsutsuki"
+let ans2 = value.slice(2,5)
+
+console.log(ans2)
+console.log(cutIt(value,2,5))
